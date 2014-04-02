a9b781baa535dc84eff441fadbc18eb3ad4076fa
Anthony <aarroyo@appliedminds.com>
2014-04-02 14:45:32 -0700

    add related entities

--- populate.py
+++ populate.py
@@ -2,22 +2,21 @@
 
 import sys, curses, os, requests, json
 
+
 from curses_menu import cmenu
 
 API_ENDPOINT = 'http://lriserver.com:6000'
 ENTITY_TYPE = ''
 
-def top():
-    os.system("top")
-
-def exit():
-    sys.exit(1)
 
 def get_things():
 
-    query = {
-        'urn:amog:property_type:ancestors': 'urn:amog:entity_type:thing'
-    }
+    query = { 'urn:amog:property_type:ancestors': 'urn:amog:entity_type:thing' }
+
+    return get_ids(query)
+
+
+def get_ids(query):
 
     r = requests.get(API_ENDPOINT + '/entity/search?q=' + json.dumps(query))
 
@@ -25,34 +24,93 @@ def get_things():
     
     return [r['props']['urn:amog:property_type:id'] for r in response]
 
-def get_thing_properties():
-    #print id
-    pass
+
+def get_thing_properties(id):
+    query = {
+        'urn:amog:property_type:id': id
+    }
+
+    r = requests.get(API_ENDPOINT + '/entity/search?q=' + json.dumps(query))
+
+    response = json.loads(r.content)['response']
+
+    try:
+        output = response[0]['props']['urn:amog:property_type:properties'] + response[0]['props']['urn:amog:property_type:specific_properties']
+    except:
+        output = response[0]['props']['urn:amog:property_type:properties']
+
+    return output
+
+
+def find_rels(type, string=None):
+    query = {
+
+        'urn:amog:property_type:types': 'urn:amog:entity_type:' + type
+
+    }
+
+    r = requests.get(API_ENDPOINT + '/entity/search?q=' + json.dumps(query))
+
+    response = json.loads(r.content)['response']
+    
+    if string:
+        output = [r['props']['urn:amog:property_type:id'] for r in response if string in r['props']['urn:amog:property_type:id']]
+    else:
+        output = [r['props']['urn:amog:property_type:id'] for r in response]
+
+    return output
+
 
 def menu(list):
     index = raw_input('\n'.join([(str(g[0]) + ' ' + g[1]) for g in enumerate(list)]) + '\n')
-    return list[int(index)]
+    try:
+        return list[int(index)]
+    except Exception:
+        return ''
+
 
 def main():
+    query = {}
+
     all_things = get_things()
 
     thing = menu(all_things)
 
-    print thing
-    
-    """
-    all_things = get_things()
+    uuid = raw_input("WHAT'S THE ID? ")
 
-    try:
-        c = cmenu([{id: get_thing_properties} for id in all_things])
-        c.display()
+    query['urn:amog:property_type:types'] = thing
 
-    except SystemExit:
-        pass
+    query['urn:amog:property_type:id'] = uuid
 
-    else:
-        c.cleanup()
-    """
+    all_props = get_thing_properties(thing)
+
+    while True:
+
+        prop = menu(all_props)
+
+        if prop:
+            val = raw_input(prop + ' --> ')
+
+            if '!!' in val:
+
+                split_val = val.split(' ')
+
+                if split_val[2]:
+
+                    val = menu(find_rels(split_val[1], split_val[2]))
+
+                else:
+
+                    val = menu(find_rels(split_val[1]))
+
+
+            query[prop] = val
+            
+        else:
+
+            break
+
+    print json.dumps(query)
 
 
 if __name__ == '__main__':
